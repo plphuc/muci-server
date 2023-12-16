8f5dc32ca10d8616b0553f41525b2fb2c0743760
plphuc <phamluumyphuc@gmail.com>
2023-12-16 10:55:59 +0700

    [add] additional commit

--- controllers/coverController.js
+++ controllers/coverController.js
@@ -1,6 +1,9 @@
+import { tokenServices } from '../services/index.js';
 import catchAsync from '../utils/catchAsync.js';
 
 const addCover = catchAsync(async (req, res, next) => {
+  const userId = tokenServices.getUserIdByToken(req)
+  
   res.status(200).send(req.file);
 });
 
