a06727ac4b560ed2b3de08c6134823ec783f4762
plmphuc <plm.phuc@tego.global>
2024-07-24 15:41:27 +0700

    fix import error

--- services/index.js
+++ services/index.js
@@ -1,6 +1,8 @@
-export * as userServices from './userServices.js'
-export * as tokenServices from './tokenServices.js'
-export * as authServices from './authServices.js'
-export * as coverServices from './coverServices.js'
-export * as pageServices from './pageServices.js'
-export * as fileServices from './fileServices.js'
+import * as userServices from './userServices.js'
+import * as tokenServices from './tokenServices.js'
+import * as authServices from './authServices.js'
+import * as coverServices from './coverServices.js'
+import * as pageServices from './pageServices.js'
+import * as fileServices from './fileServices.js'
+
+export {userServices, tokenServices, authServices, coverServices, pageServices, fileServices}
--- validations/index.js
+++ validations/index.js
@@ -1,4 +1,6 @@
-export * as authValidate from './authValidation.js';
-export * as pageValidate from './pageValidation.js';
-export * as customValidate from './customValidation.js';
-export * as coverValidate from './coverValidation.js';
+import * as authValidate from './authValidation.js';
+import * as pageValidate from './pageValidation.js';
+import * as customValidate from './customValidation.js';
+import * as coverValidate from './coverValidation.js';
+
+export {authValidate, pageValidate, customValidate, coverValidate}
